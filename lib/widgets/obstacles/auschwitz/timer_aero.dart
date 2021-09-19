15ddc39955dc9ac144d3ccca13f2150a946811b3
unknown <aminehajali@insat.u-carthage.tn>
2021-09-19 21:07:39 +0100

    added a timer

--- lib/widgets/obstacles/auschwitz/timer_aero.dart
+++ lib/widgets/obstacles/auschwitz/timer_aero.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:aerochallenge_app/config/responsive_size.dart';
 import 'package:aerochallenge_app/config/theme.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +14,13 @@ class TimerAero extends StatefulWidget {
 }
 
 class _TimerAeroState extends State<TimerAero> {
+  Stream<int> timerStream;
+  StreamSubscription<int> timerSubscription;
+  String hoursStr = "00";
+  String minutesStr = "00";
+  String secondsStr = "00";
+  bool active = false;
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -24,29 +33,101 @@ class _TimerAeroState extends State<TimerAero> {
               WidgetSpan(
                 child: Icon(
                   Icons.play_arrow,
-                  size: 24,
+                  size: SizeConfig.defaultSize * 2.4,
                 ),
               ),
-              TextSpan(
-                text: 'niveau 1',
-                style: TextStyle(
-                  color: LIGHT_COLOR,
-                  fontSize: SizeConfig.defaultSize * 2,
+              WidgetSpan(
+                  child: SizedBox(
+                width: SizeConfig.defaultSize / 2,
+              )),
+              WidgetSpan(
+                child: Center(
+                  child: Text(
+                    'Niveau 1',
+                    style: TextStyle(
+                      color: LIGHT_COLOR,
+                      fontSize: SizeConfig.defaultSize * 2,
+                    ),
+                  ),
                 ),
               )
             ]),
           ),
-          color: AERO_RED,
+          color: active ? AERO_YELLOW : AERO_RED,
           width: SizeConfig.screenWidth * 0.38,
           height: SizeConfig.defaultSize * 5,
-          onPressed: () {},
+          onPressed: active
+              ? () {
+                  timerSubscription.cancel();
+                  timerStream = null;
+                  setState(() {
+                    hoursStr = '00';
+                    minutesStr = '00';
+                    secondsStr = '00';
+                    active = !active;
+                  });
+                }
+              : () {
+                  setState(() {
+                    active = !active;
+                  });
+                  timerStream = stopWatchStream();
+                  timerSubscription = timerStream.listen((int newTick) {
+                    setState(() {
+                      hoursStr = ((newTick / (60 * 60)) % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      minutesStr = ((newTick / 60) % 60)
+                          .floor()
+                          .toString()
+                          .padLeft(2, '0');
+                      secondsStr =
+                          (newTick % 60).floor().toString().padLeft(2, '0');
+                    });
+                  });
+                },
         ),
         Text(
-          '00:00:00',
+          "$hoursStr:$minutesStr:$secondsStr",
           style: TextStyle(
               fontSize: SizeConfig.defaultSize * 1.8, color: AERO_BLUE),
         ),
       ],
     );
+  }
+
+  Stream<int> stopWatchStream() {
+    StreamController<int> streamController;
+    Timer timer;
+    Duration timerInterval = Duration(seconds: 1);
+    int counter = 0;
+
+    void stopTimer() {
+      if (timer != null) {
+        timer.cancel();
+        timer = null;
+        counter = 0;
+        streamController.close();
+      }
+    }
+
+    void tick(_) {
+      counter++;
+      streamController.add(counter);
+    }
+
+    void startTimer() {
+      timer = Timer.periodic(timerInterval, tick);
+    }
+
+    streamController = StreamController<int>(
+      onListen: startTimer,
+      onCancel: stopTimer,
+      onResume: startTimer,
+      onPause: stopTimer,
+    );
+
+    return streamController.stream;
   }
 }
