bfb82d50ae7668e8365d07ee98c41903c3f97581
unknown <aminehajali@insat.u-carthage.tn>
2021-09-22 22:38:18 +0100

    fixed timer

--- lib/widgets/obstacles/auschwitz/timer_aero.dart
+++ lib/widgets/obstacles/auschwitz/timer_aero.dart
@@ -14,12 +14,61 @@ class TimerAero extends StatefulWidget {
 }
 
 class _TimerAeroState extends State<TimerAero> {
-  Stream<int> timerStream;
-  StreamSubscription<int> timerSubscription;
+  Stopwatch stopwatch = new Stopwatch();
   String minutesStr = "00";
   String secondsStr = "00";
   String millisecondsStr = "00";
+  IconData icon = Icons.play_arrow;
+  bool shouldPlay = true;
   bool active = false;
+  Timer timer;
+  Duration timerInterval = Duration(milliseconds: 10);
+  int counter = 0;
+
+  stopTimer() {
+    setState(() {
+      active = false;
+      shouldPlay = false;
+      icon = Icons.replay;
+      stopwatch.stop();
+      setTime();
+    });
+  }
+
+  setTime() {
+    int time = stopwatch.elapsedMilliseconds;
+    setState(() {
+      minutesStr = (time / (1000 * 60)).floor().toString().padLeft(2, '0');
+      secondsStr = (time / 1000).floor().toString().padLeft(2, '0');
+      millisecondsStr = ((time / 10) % 100).floor().toString().padLeft(2, '0');
+    });
+  }
+
+  updateTimer(Timer t) {
+    if (stopwatch.isRunning) setTime();
+  }
+
+  resetWatch() {
+    setState(() {
+      stopwatch.reset();
+      minutesStr = "00";
+      secondsStr = "00";
+      millisecondsStr = "00";
+      active = false;
+      shouldPlay = true;
+      icon = Icons.play_arrow;
+    });
+  }
+
+  startTimer() {
+    setState(() {
+      stopwatch.start();
+      shouldPlay = false;
+      active = true;
+      timer = Timer.periodic(timerInterval, updateTimer);
+      icon = Icons.stop;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,68 +76,44 @@ class _TimerAeroState extends State<TimerAero> {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         AeroButton(
-          content: RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(children: [
-              WidgetSpan(
-                child: Icon(
-                  active ? Icons.stop : Icons.play_arrow ,
-                  size: SizeConfig.defaultSize * 2.4,
-                ),
-              ),
-              WidgetSpan(
-                  child: SizedBox(
-                width: SizeConfig.defaultSize / 2,
-              )),
-              WidgetSpan(
-                child: Center(
-                  child: Text(
-                    'Niveau 1',
-                    style: TextStyle(
-                      color: LIGHT_COLOR,
-                      fontSize: SizeConfig.defaultSize * 2,
-                    ),
+            content: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(children: [
+                WidgetSpan(
+                  child: Icon(
+                    icon,
+                    size: SizeConfig.defaultSize * 2.4,
                   ),
                 ),
-              )
-            ]),
-          ),
-          color: active ? AERO_YELLOW : AERO_RED,
-          width: SizeConfig.screenWidth * 0.38,
-          height: SizeConfig.defaultSize * 5,
-          //https://medium.com/analytics-vidhya/build-a-simple-stopwatch-in-flutter-a1f21cfcd7a8
-          onPressed: active
-              ? () {
-                  timerSubscription.cancel();
-                  timerStream = null;
-                  setState(() {
-                    minutesStr = '00';
-                    secondsStr = '00';
-                    millisecondsStr = '00';
-                    active = !active;
-                  });
-                }
-              : () {
-                  setState(() {
-                    active = !active;
-                  });
-                  timerStream = stopWatchStream();
-                  timerSubscription = timerStream.listen((int newTick) {
-                    setState(() {
-                      minutesStr = ((newTick / (100 * 60)) % 60)
-                          .floor()
-                          .toString()
-                          .padLeft(2, '0');
-                      secondsStr = ((newTick / 100) % 60)
-                          .floor()
-                          .toString()
-                          .padLeft(2, '0');
-                      millisecondsStr =
-                          (newTick % 100).floor().toString().padLeft(2, '0');
-                    });
-                  });
-                },
-        ),
+                WidgetSpan(
+                    child: SizedBox(
+                  width: SizeConfig.defaultSize / 2,
+                )),
+                WidgetSpan(
+                  child: Center(
+                    child: Text(
+                      'Niveau 1',
+                      style: TextStyle(
+                        color: LIGHT_COLOR,
+                        fontSize: SizeConfig.defaultSize * 2,
+                      ),
+                    ),
+                  ),
+                )
+              ]),
+            ),
+            color: active ? AERO_YELLOW : AERO_RED,
+            width: SizeConfig.screenWidth * 0.38,
+            height: SizeConfig.defaultSize * 5,
+            //https://medium.com/analytics-vidhya/build-a-simple-stopwatch-in-flutter-a1f21cfcd7a8
+            onPressed: active
+                ? stopTimer
+                : () {
+                    if (shouldPlay)
+                      startTimer();
+                    else
+                      resetWatch();
+                  }),
         Text(
           "$minutesStr:$secondsStr:$millisecondsStr",
           style: TextStyle(
@@ -96,39 +121,5 @@ class _TimerAeroState extends State<TimerAero> {
         ),
       ],
     );
-  }
-
-  Stream<int> stopWatchStream() {
-    StreamController<int> streamController;
-    Timer timer;
-    Duration timerInterval = Duration(milliseconds: 10);
-    int counter = 0;
-
-    void stopTimer() {
-      if (timer != null) {
-        timer.cancel();
-        timer = null;
-        counter = 0;
-        streamController.close();
-      }
-    }
-
-    void tick(_) {
-      counter++;
-      streamController.add(counter);
-    }
-
-    void startTimer() {
-      timer = Timer.periodic(timerInterval, tick);
-    }
-
-    streamController = StreamController<int>(
-      onListen: startTimer,
-      onCancel: stopTimer,
-      onResume: startTimer,
-      onPause: stopTimer,
-    );
-
-    return streamController.stream;
   }
 }
